c0cad3bb6b9c2b40639f53a355661e97fbe99ea4
Bagus Nugroho <papaobobo@gmail.com>
2023-06-20 12:00:09 +0700

    initial commit & setup

---
+++ todo_app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-neutral-dark-200 h-screen w-screen'>
+
+    </div>
+  )
+}
+
+export default App
